ca2c2b7aad6a751e719a34ac4b938be8844deab4
Marc van Dijk <marcvdijk@gmail.com>
2019-05-29 15:14:02 +0200

    Fix typo in classifiers

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :; 3',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Chemistry'
